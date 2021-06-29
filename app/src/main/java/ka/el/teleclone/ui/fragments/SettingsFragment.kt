201701ca4d151de18ed262e05ea0870e73bd0f7b
Ekaterina-El <hackerrussia@yandex.ru>
2021-06-30 00:38:59 +0500

    Added option menu on SettingsFragment

--- app/src/main/java/ka/el/teleclone/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/ka/el/teleclone/ui/fragments/SettingsFragment.kt
@@ -1,16 +1,20 @@
 package ka.el.teleclone.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import ka.el.teleclone.R
 import ka.el.teleclone.databinding.FragmentChatBinding
 import ka.el.teleclone.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
+
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_menu, menu)
     }
 }
