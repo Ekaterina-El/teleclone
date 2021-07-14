38a782ef1c0b851006c69852fa5f2b549a84eaa2
Ekaterina-El <hackerrussia@yandex.ru>
2021-07-14 15:27:18 +0500

    Added util for control media recorder

---
+++ app/src/main/java/ka/el/teleclone/utils/AppVoiceRecorder.kt
@@ -0,0 +1,61 @@
+package ka.el.teleclone.utils
+
+import android.media.MediaRecorder
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.io.File
+
+class AppVoiceRecorder {
+    private var mMediaRecorder = MediaRecorder()
+    private lateinit var mMessageKey: String
+    private lateinit var mFile: File
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun startRecord(messageKey: String) {
+        try {
+            mMessageKey = messageKey
+            createFileForRecord()
+            prepareMediaRecorder()
+            mMediaRecorder.start()
+        } catch (e: Exception) {
+            showToast(e.message.toString())
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun prepareMediaRecorder() {
+
+        mMediaRecorder.apply {
+            reset()
+            setOutputFormat(MediaRecorder.OutputFormat.DEFAULT)
+            setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+            setAudioSource(MediaRecorder.AudioSource.DEFAULT)
+            setOutputFile(mFile.absoluteFile)
+            prepare()
+        }
+    }
+
+    private fun createFileForRecord() {
+        mFile = File(APP_ACTIVITY.filesDir, mMessageKey)
+        mFile.createNewFile()
+    }
+
+    fun stopRecord(onSuccess: (File, String) -> Unit) {
+        try {
+            mMediaRecorder.stop()
+            onSuccess(mFile, mMessageKey)
+        } catch (e: Exception) {
+            showToast(e.message.toString())
+            mFile.delete()
+        }
+    }
+
+    fun releaseRecord() {
+        try {
+            mMediaRecorder.release()
+        } catch (e: Exception) {
+            showToast(e.message.toString())
+        }
+    }
+
+}
